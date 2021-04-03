373a739dedca23b6ca17b8825ae93b311fa4f2f3
Nur-allhi <noorefty1@gmail.com>
2021-04-03 22:18:36 +0600

    increase Decrease work done

--- script.js
+++ script.js
@@ -1,25 +1,107 @@
-document.getElementById("fc-plus-btn").addEventListener("click", function () {
-  const fcCurrentInput = document.getElementById("fc-current-input").value;
-  const fcCurrentInputNumber = parseInt(fcCurrentInput);
-  const fcInputShow = fcCurrentInputNumber + 1;
-  document.getElementById("fc-current-input").value = fcInputShow;
-  const ticketPrice = fcInputShow * 150;
+// fc= Firstclass
 
-  const currentSubTotal = document.getElementById("sub-Total").innerText;
-  const currentSubTotalNumber = parseInt(currentSubTotal);
-  const subTotal = ticketPrice + currentSubTotalNumber;
-  document.getElementById("sub-Total").innerText = subTotal;
+// Firstclass ticket selection
+document.getElementById("fc-plus-btn").addEventListener("click", function () {
+  fcticket(true);
+  // const fcCurrentInput = document.getElementById("fc-current-input");
+  // const fcCurrentInputNumber = parseInt(fcCurrentInput.value);
+  // const fcNewInput = fcCurrentInputNumber + 1;
+  // fcCurrentInput.value = fcNewInput;
+  // const ticketPrice = fcNewInput * 150;
+  // document.getElementById("sub-Total").innerText = ticketPrice;
 });
 
 document.getElementById("fc-minus-btn").addEventListener("click", function () {
-  const fcCurrentInput = document.getElementById("fc-current-input").value;
-  const fcCurrentInputNumber = parseInt(fcCurrentInput);
-  const fcInputShow = fcCurrentInputNumber - 1;
-  document.getElementById("fc-current-input").value = fcInputShow;
-  const ticketPrice = fcInputShow * 150;
-
-  const currentSubTotal = document.getElementById("sub-Total").innerText;
-  const currentSubTotalNumber = parseInt(currentSubTotal);
-  const subTotal = currentSubTotalNumber - ticketPrice;
-  document.getElementById("sub-Total").innerText = subTotal;
+  fcticket(false);
+  // const fcCurrentInput = document.getElementById("fc-current-input");
+  // const fcCurrentInputNumber = parseInt(fcCurrentInput.value);
+  // if (fcCurrentInputNumber > 0) {
+  //   const fcNewInput = fcCurrentInputNumber - 1;
+  //   fcCurrentInput.value = fcNewInput;
+  //   const ticketPrice = fcNewInput * 150;
+  //   document.getElementById("sub-Total").innerText = ticketPrice;
+  // }
 });
+
+// Increase & Decrease function (FirstClass ticket)
+function fcticket(isIncrease) {
+  const fcCurrentInput = document.getElementById("fc-current-input");
+  const fcCurrentInputNumber = parseInt(fcCurrentInput.value);
+  let fcNewInput = fcCurrentInputNumber;
+  if (isIncrease == true) {
+    fcNewInput = fcCurrentInputNumber + 1;
+  }
+  if (isIncrease == false && fcCurrentInputNumber > 0) {
+    fcNewInput = fcCurrentInputNumber - 1;
+  }
+  fcCurrentInput.value = fcNewInput;
+  const fcticketPrice = fcNewInput * 150;
+  document.getElementById("sub-Total").innerText = fcticketPrice;
+}
+// ------------------------------------------------------------------------------------
+
+// Economy class ticket selection
+
+document.getElementById("ec-plus-btn").addEventListener("click", function () {
+  ecticket(true);
+});
+
+document.getElementById("ec-minus-btn").addEventListener("click", function () {
+  ecticket(false);
+});
+
+// Increase & Decrease function (EconomyClass ticket)
+function ecticket(isIncrease) {
+  const ecCurrentInput = document.getElementById("ec-current-input");
+  const ecCurrentInputNumber = parseInt(ecCurrentInput.value);
+  let ecNewInput = ecCurrentInputNumber;
+  if (isIncrease == true) {
+    ecNewInput = ecCurrentInputNumber + 1;
+  }
+  if (isIncrease == false && ecCurrentInputNumber > 0) {
+    ecNewInput = ecCurrentInputNumber - 1;
+  }
+  ecCurrentInput.value = ecNewInput;
+  const ecticketPrice = ecNewInput * 100;
+  document.getElementById("sub-Total").innerText = ecticketPrice;
+}
+// -------------------------------------------------------------------------
+
+// Common fnction for Increase decrease
+
+// function economyClassTicket(isIncrease) {
+//   const CurrentInput = document.getElementById("current-input");
+//   const CurrentInputNumber = parseInt(CurrentInput.value);
+//   let NewInput = CurrentInputNumber;
+//   if (isIncrease == true) {
+//     NewInput = CurrentInputNumber + 1;
+//   }
+//   if (isIncrease == false && CurrentInputNumber > 0) {
+//     NewInput = CurrentInputNumber - 1;
+//   }
+//   CurrentInput.value = NewInput;
+//   const ticketPrice = NewInput * 100;
+//   document.getElementById("sub-Total").innerText = ticketPrice;
+// }
+
+// // test
+// function fcticket(isIncrease) {
+//   const fcCurrentInput = document.getElementById("fc-current-input");
+//   const fcCurrentInputNumber = parseInt(fcCurrentInput.value);
+//   let fcNewInput = fcCurrentInputNumber;
+//   if (isIncrease == true) {
+//     fcNewInput = fcCurrentInputNumber + 1;
+//   }
+//   if (isIncrease == false && fcCurrentInputNumber > 0) {
+//     fcNewInput = fcCurrentInputNumber - 1;
+//   }
+//   fcCurrentInput.value = fcNewInput;
+//   const fcticketPrice = fcNewInput * 150;
+//   const CurrentSubtotal = document.getElementById("sub-total");
+//   const currentSubtotalNumber = parseInt(CurrentSubtotal.value);
+//   CurrentSubtotal.value = subtotalFinalValue;
+//   const subtotalFinalValue = fcticketPrice + currentSubtotalNumber;
+
+//   document.getElementById("sub-Total").innerText = subtotalFinalValue;
+// }
+// // test end
